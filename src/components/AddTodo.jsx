f965f9a982f0e9a97299f0563f14bbc7a93c558a
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-15 22:40:53 +0530

    add useState for title and Desc fields

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import '../index.css';
 
 export default function AddTodo() {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   return (
     <div>AddTodo</div>
   )
