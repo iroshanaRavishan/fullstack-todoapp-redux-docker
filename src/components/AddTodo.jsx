2fcc175bee35fc62548f8af2a00e132e0a721c35
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-16 22:36:33 +0530

    add useState for status

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -4,6 +4,7 @@ import '../index.css';
 export default function AddTodo() {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
+  const [status, setStatus] = useState('');
   return (
     <div className="todo-form-container">
       <h2>Add Todo</h2>
