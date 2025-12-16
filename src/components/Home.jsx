2e656e3af030eb13e0a8c7bb37ac56895b814493
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-16 12:17:02 +0530

    add AddTodo component to Home for todo creation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import List from './List'
+import AddTodo from './AddTodo'
 
 export default function Home() {
   return (
     <>
       <List />
+      <AddTodo />
     </>
   )
 }
